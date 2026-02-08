e1391a61e70913cf7ace6e37df804c17678f8d32
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-07 17:35:25 -0800

    added the tracking variables and inserted their rules

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // This will keep track of the Tracking variables 
+            int currentStep = 0;       // Keeps track on the current step you're in 
+            int maximumSteps = 10;     // Maximum number of steps it could reach
+            int restrictedSteps = 3;   // This indicates the Restriced Steps (Skip Behavior)
+            int safetyLimit = 7;       // This indicates the saftey stop
+            int choice = 0;
+            bool ifRunning = true;
+
             Console.WriteLine("");
         }
     }
