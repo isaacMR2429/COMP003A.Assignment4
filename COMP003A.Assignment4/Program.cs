7eee70a47f773b50c77c6c6f4271fa7f87f91ac0
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-07 18:38:09 -0800

    case 1 added with the rules

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -6,7 +6,7 @@
         {
             // This will keep track of the Tracking variables 
             int currentStep = 0;       // Keeps track on the current step you're in 
-            int maximumSteps = 10;     // Maximum number of steps it could reach
+            int maximumStep = 10;     // Maximum number of steps it could reach
             int restrictedSteps = 3;   // This indicates the Restriced Steps (Skip Behavior)
             int safetyLimit = 7;       // This indicates the saftey stop
             int choice = 0;
@@ -19,6 +19,29 @@
                 Console.WriteLine("Choice 3: Exit");
                 Console.Write("Enter Choice: ");
                 choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        currentStep++;
+                        if (currentStep == restrictedSteps)
+                        {
+                            Console.WriteLine($"Step {currentStep} is restrcited and was skipped");
+                            continue;
+                        }
+                        if (currentStep == safetyLimit)
+                        {
+                            Console.WriteLine("Saftey limit reached. Routine stopped.");
+                            break;
+                        }
+                        if (currentStep > maximumStep)
+                        {
+                            Console.WriteLine("Maximum Steps has been reached. Routine is completed");
+                            break; 
+                        }
+                        Console.WriteLine($"Routine Step {currentStep} completed.");
+                        break;
+                }
             }
         }
     }
